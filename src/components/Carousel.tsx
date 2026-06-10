6d868dff0ac5bce5912f17416c1102f9294d8fcd
Non-territorial <i@nonterritorial.art>
2026-06-10 10:28:13 +0200

    isocpeur

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -46,7 +46,7 @@ export default function Carousel({ work, onClose }: CarouselProps) {
 <div className={slide.type === 'image' ? 'fixed inset-0 pointer-events-auto' : 'w-full md:w-auto max-w-4xl px-6 md:px-0 pointer-events-auto'}>
   {slide.type === 'text' && (
   <div
-    className={`text-white/70 leading-relaxed whitespace-pre-wrap text-sm md:text-base lg:text-base w-full overflow-hidden wrap-break-word max-h-[80vh] overflow-y-auto md:max-h-none md:overflow-visible`} style={{ fontFamily: 'iAWriterDuoS, monospace' }}
+    className={`text-white/70 leading-relaxed whitespace-pre-wrap text-lg md:text-xl lg:text-2xl w-full overflow-hidden wrap-break-word max-h-[80vh] overflow-y-auto md:max-h-none md:overflow-visible`} style={{ fontFamily: 'isocpeur, monospace' }}
   >
     {slide.title && (
       <div className="text-white/70 text-xl md:text-2xl lg:text-3xl mb-2" style={{ fontFamily: 'isocpeur' }}>
@@ -136,13 +136,13 @@ export default function Carousel({ work, onClose }: CarouselProps) {
         />
       </div>
     )}
-    <div className="text-white/70 leading-relaxed whitespace-pre-wrap text-sm md:text-base lg:text-base max-w-lg lg:max-w-xl flex flex-col justify-center">
+    <div className="text-white/70 leading-relaxed whitespace-pre-wrap text-lg md:text-xl lg:text-2xl max-w-lg lg:max-w-xl flex flex-col justify-center">
       {slide.title && (
         <div className="text-xl md:text-2xl lg:text-3xl mb-4" style={{ fontFamily: 'isocpeur' }}>
           {slide.title}
         </div>
       )}
-    <div style={{ fontFamily: 'iAWriterDuoS, monospace' }}
+    <div style={{ fontFamily: 'isocpeur, monospace' }}
   dangerouslySetInnerHTML={{
     __html: slide.content
       .replace(/\n/g, '<br />')
